3abaea4fcaf795dbc3433e73a222a8e3cf6e4758
Owen Xi <junjunconan@gmail.com>
2023-09-28 18:09:13 +1000

    Add spec for db

--- app/infra/database.rb
+++ app/infra/database.rb
@@ -21,28 +21,28 @@ module Infra
       @instance = new
     end
 
-    def insert_record(attrs, table)
-      table = Database.instance.send(table)
-      raise StandardError.new('Table not found') if table.nil?
+    def insert_record(attrs, table_name)
+      raise StandardError.new('Table not found') unless Database.instance.respond_to?(table_name)
 
+      table = Database.instance.send(table_name)
       table << attrs
       attrs
     end
 
-    def update_record(attrs, table)
-      table = Database.instance.send(table)
-      raise StandardError.new('Table not found') if table.nil?
+    def update_record(attrs, table_name)
+      raise StandardError.new('Table not found') unless Database.instance.respond_to?(table_name)
 
+      table = Database.instance.send(table_name)
       index = table.find_index { |existing_record| existing_record[:uuid] == attrs[:uuid] }
       raise StandardError.new('Record not found') if index.nil?
 
       table[index] = attrs
     end
 
-    def find_record_by(attrs, table)
-      table = Database.instance.send(table)
-      raise StandardError.new('Table not found') if table.nil?
+    def find_record_by(attrs, table_name)
+      raise StandardError.new('Table not found') unless Database.instance.respond_to?(table_name)
 
+      table = Database.instance.send(table_name)
       table.find { |record| record >= (attrs || {}) }
     end
   end
---
+++ spec/infra/database_spec.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Infra::Database do
+  describe '.instance' do
+    it 'should return the same instance' do
+      instance_1 = Infra::Database.instance
+      instance_2 = Infra::Database.instance
+
+      expect(instance_1).to be(instance_2)
+    end
+  end
+
+  describe '.clear' do
+    it 'should clear and re-initialize the instance' do
+      instance_1 = Infra::Database.instance
+      Infra::Database.clear
+      instance_2 = Infra::Database.instance
+
+      expect(instance_1).not_to be(instance_2)
+    end
+  end
+
+  describe '#insert_record' do
+    let(:database) { Infra::Database.instance }
+    let(:attrs) { { uuid: 'uuid' } }
+
+    it 'should insert a record to the table' do
+      database.insert_record(attrs, :bank_accounts)
+      expect(database.bank_accounts).to eq [attrs]
+    end
+
+    it 'should raise an error when table is not found' do
+      expect { database.insert_record(attrs, :non_existing_table) }.to raise_error(StandardError, 'Table not found')
+    end
+  end
+
+  describe '#update_record' do
+    let(:database) { Infra::Database.instance }
+    let(:attrs) { { uuid: 'uuid', name: 'name' } }
+
+    before { database.bank_accounts << { uuid: 'uuid' } }
+
+    it 'should updates the record' do
+      database.update_record(attrs, :bank_accounts)
+      expect(database.bank_accounts).to eq [attrs]
+    end
+
+    it 'should raise an error when record is not found' do
+      expect { database.update_record({ uuid: 'wrong_uuid' }, :bank_accounts) }.to raise_error(StandardError, 'Record not found')
+    end
+
+    it 'should raise an error when table is not found' do
+      expect { database.update_record(attrs, :non_existing_table) }.to raise_error(StandardError, 'Table not found')
+    end
+  end
+
+  describe '#find_record_by' do
+    let(:database) { Infra::Database.instance }
+    let(:attrs) { { uuid: 'uuid' } }
+
+    before { database.bank_accounts << attrs }
+
+    it 'should find the record' do
+      expect(database.find_record_by(attrs, :bank_accounts)).to eq attrs
+    end
+
+    it 'should return nil' do
+      expect(database.find_record_by({ uuid: 'another_uuid' }, :bank_accounts)).to be_nil
+    end
+
+    it 'should raise an error when table is not found' do
+      expect { database.find_record_by(attrs, :non_existing_table) }.to raise_error(StandardError, 'Table not found')
+    end
+  end
+end
